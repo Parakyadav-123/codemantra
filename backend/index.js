95c99a53c7f746cf220ccd4d7abbb19b5735ac62
Parak Yadav <parakydv@gmail.com>
2026-01-09 15:59:02 +0530

    Project files

---
+++ backend/index.js
@@ -0,0 +1,138 @@
+import express from "express";
+import http from 'http';
+import { Server } from "socket.io";
+import path from "path";
+import { version } from "os";
+import axios from "axios";
+
+
+const app = express();
+
+const server = http.createServer(app);
+
+// //Save time while opening again
+// const url = `https://code-mantra-ylgq.onrender.com/`;
+// const interval = 30000;
+
+// function reloadWebsite() {
+//   axios
+//     .get(url)
+//     .then((response) => {
+//       console.log("website reloded");
+//     })
+//     .catch((error) => {
+//       console.error(`Error : ${error.message}`);
+//     });
+// }
+
+// setInterval(reloadWebsite, interval);
+
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+const rooms = new Map();
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    let currentRoom = null;
+    let currentUser = null;
+
+
+    socket.on("join", ({roomId, userName}) => {
+        if(currentRoom){
+            socket.leave(currentRoom);
+            rooms.get(currentRoom).users.delete(currentUser);
+            io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom).users))
+        }
+
+        currentRoom = roomId;
+        currentUser = userName;
+
+        socket.join(roomId);
+
+        if(!rooms.has(roomId)){
+            rooms.set(roomId, {users: new Set(), code: "//start coding here"});
+        }
+
+        rooms.get(roomId).users.add(userName);
+
+        socket.emit("codeUpdate", rooms.get(roomId).code);
+
+        io.to(roomId).emit("userJoined", Array.from(rooms.get(currentRoom).users));
+    })
+
+    socket.on("codeChange", ({ roomId,code }) => {
+        if(rooms.has(roomId)){
+            rooms.get(roomId).code = code;
+        }
+        socket.to(roomId).emit("codeUpdate", code)
+    })
+
+
+    socket.on("leaveRoom", () => {
+        if(currentRoom && currentUser){
+            rooms.get(currentRoom).users.delete(currentUser);
+            io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom).users));
+
+            socket.leave(currentRoom)
+            currentRoom = null;
+            currentUser = null;
+        }
+    })
+
+    socket.on("typing", ({roomId, userName}) => {
+        socket.to(roomId).emit("userTyping", userName);
+    })
+
+    socket.on("languageChange", ({roomId, language}) => {
+        io.to(roomId).emit("languageUpdate", language);
+    })
+
+    //Adding compiler
+    //Using piston api
+    socket.on("compileCode", async({code, roomId, language, version}) => {
+        if(rooms.has(roomId)){
+            const room = rooms.get(roomId);
+            const response = await axios.post("https://emkc.org/api/v2/piston/execute", {
+                language,
+                version,
+                files: [
+                    {
+                        content: code
+                    }
+                ]
+            })
+
+            room.output = response.data.run.output
+            io.to(roomId).emit("codeResponse", response.data);
+        }
+    })
+
+    socket.on("disconnect", () => {
+        if(currentRoom && currentUser){
+            rooms.get(currentRoom).users.delete(currentUser);
+            io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom).users));
+        }
+        console.log(`User disconnected!!`);
+    })
+})
+
+
+const port = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
+
+server.listen(port, () => {
+    console.log(`Server is working on port ${port}`);
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,210 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './App.css';
+import io from 'socket.io-client'
+import { useState, useEffect, useMemo } from 'react';
+import Editor from '@monaco-editor/react';
+import {v4 as uuid} from 'uuid'
+
+const socket = io('http://localhost:3000');
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [joined, setJoined] = useState(false);
+  const [roomId, setRoomId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [language, setLanguage] = useState('cpp');
+  const [code,setCode] = useState('//Start coding here');
+  const [copySuccess, setCopySuccess] = useState("");
+  const [users, setUsers] = useState([]);
+  const [typing, setTyping] = useState("");
+  const [output, setOutput] = useState("");
+  const [version, setVersion] = useState("*");
+
+  useEffect( () => {
+    socket.on("userJoined", (users) => {
+      setUsers(users);
+    });
+
+    socket.on("codeUpdate", (newCode) => {
+      setCode(newCode);
+    })
+
+    socket.on("userTyping", (user) => {
+      setTyping(`${user} is typing..`)
+      setTimeout( () => {
+        setTyping("");
+      },3000)
+    })
+
+    socket.on("languageUpdate", (newLanguage) => {
+      setLanguage(newLanguage);
+    })
+
+    socket.on("codeResponse", (response) => {
+      setOutput(response.run.output);
+    })
+
+    return () => {
+      socket.off("userJoined");
+      socket.off("codeUpdate");
+      socket.off("userTyping");
+      socket.off("languageUpdate");
+      socket.off("codeResponse");
+    }
+  }, [])
+
+  useEffect( () => {
+    const handleBeforeUnload = () => {
+      socket.emit("leaveRoom");
+    }
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload",handleBeforeUnload);
+    }
+  }, [])
+
+  const joinRoom = () => {
+    if(roomId && userName){
+      socket.emit("join", {roomId, userName});
+      setJoined(true);
+    }
+  }
+
+  const leaveRoom = () => {
+    socket.emit("leaveRoom");
+    setJoined(false);
+    setRoomId("");
+    setUserName("");
+    setCode("//Start coding here");
+    setLanguage("cpp");
+  }
+
+  const copyRoomId = () => {
+    navigator.clipboard.writeText(roomId)
+    setCopySuccess("Copied to clipboard!");
+    setTimeout( () => {
+      setCopySuccess("")
+    },2000);
+  }
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+    socket.emit("codeChange", {roomId, code: newCode});
+    socket.emit("typing", {roomId, userName});
+  }
+
+  const handleLanguageChange = (e) => {
+    const newLanguage = e.target.value;
+    setLanguage(newLanguage);
+    socket.emit("languageChange", {roomId, language: newLanguage});
+  }
+
+  //Run code
+  const runCode = () => {
+    socket.emit("compileCode", {
+      code,
+      roomId,
+      language,
+      version
+    })
+  }
+
+  //Using 'uuid' to generate unique room id
+  const createRoomId = () => {
+    const roomId = uuid()
+    setRoomId(roomId)
+  }
+
+
+  if(!joined){
+    return(
+      <div className="join-container">
+        <div className="join-form">
+          <h1>Code Mantra</h1>
+          <input 
+          type="text" 
+          placeholder='Room Id'
+          value = {roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+          />
+
+          <input 
+          type="text" 
+          placeholder='Your Name'
+          value = {userName}
+          onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <button onClick={joinRoom} className='join-room-button'>Join Room</button>
+
+          <div className='generate-room'>
+            If don't have room Id
+            <button className='generate-room-button' onClick={createRoomId}>Create Room</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    )
+  }
+
+  return <div className='editor-container'>
+    <div className="sidebar">
+      
+      <div className="room-info">
+        <h1>Code Mantra</h1>
+        <h2>Room ID: {roomId}</h2>
+        <button className='copy-button' onClick={copyRoomId}>Copy Room ID</button>
+        {copySuccess && <span className='copy-success'>{copySuccess}</span>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <h3>Users in Room:</h3>
+      
+      <ul>
+        {
+          users.map((user,index) => (
+            <li key={index}>{user.slice(0,8)}</li>
+          ))
+        }
+      </ul>
+      
+      <p className='typing-indicator'> {typing} </p>
+      
+      <select className='language-select' value={language} onChange = { handleLanguageChange }>
+        <option value="javascript">JavaScript</option>
+        <option value="python">Python</option>
+        <option value="cpp">C++</option>
+        <option value="java">Java</option>
+      </select>
+      
+      <button className='leave-button' onClick={leaveRoom}>Leave Room</button>
+    </div>
+    
+    <div className="editor-wrapper">
+      <Editor 
+        height={"60vh"} 
+        defaultLanguage = {language} 
+        language={language} 
+        value={code}
+        onChange={handleCodeChange}
+        theme="vs"
+        options= {
+          {
+            minimap: {enabled: false},
+            fontSize: 14
+          }
+        }
+      />
+      
+      <button className='run-btn' onClick={runCode}>Run</button>
+      <textarea className='output-console' value={output} readOnly placeholder='Output Will appear here...'></textarea>
+    </div>
+
+  </div>
+
+  
 }
 
 export default App
